a2b59dd789364c240a25bc02d9febd1e40d840f5
missgoin <90597569+missgoin@users.noreply.github.com>
2023-05-28 10:41:02 +0700

    Update merlin.sh

--- .circleci/merlin.sh
+++ .circleci/merlin.sh
@@ -25,6 +25,8 @@ function exports() {
   export KBUILD_BUILD_USER="unknown"
   export PROCS=$(nproc --all)
   export DISTRO=$(source /etc/os-release && echo "${NAME}")
+  export LC_ALL=C && export USE_CCACHE=1
+  ccache -M 100G
 
 # Variables
 KERVER=$(make kernelversion)
@@ -33,7 +35,7 @@ DATE=$(TZ=Asia/Jakarta date +"%Y%m%d-%T")
 TANGGAL=$(date +"%F%S")
 
 # Compiler and Build Information
-TOOLCHAIN=azure # List (clang = nexus14 | aosp | nexus15 | proton )
+TOOLCHAIN=neutron # List (clang = nexus14 | aosp | nexus15 | proton )
 #LINKER=ld # List ( ld.lld | ld.bfd | ld.gold | ld )
 VERBOSE=0
 
@@ -59,10 +61,15 @@ elif [[ $TOOLCHAIN == "proton" ]]; then
        git clone --depth=1 https://github.com/kdrag0n/proton-clang clang
 elif [[ $TOOLCHAIN == "nexus15" ]]; then
        git clone --depth=1 https://gitlab.com/Project-Nexus/nexus-clang.git -b nexus-15 clang
+elif [[ $TOOLCHAIN == "neutron" ]]; then
+bash <(curl -s https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman) -S=latest
+apt install libelf-dev libarchive-tools
+bash -c "$(wget -O - https://gist.githubusercontent.com/dakkshesh07/240736992abf0ea6f0ee1d8acb57a400/raw/e97b505653b123b586fc09fda90c4076c8030732/patch-for-old-glibc.sh)"
+
 fi
 
 # Get AnyKernel3
-git clone --depth=1 https://github.com/reaPeR1010/AnyKernel3 AK3
+git clone --depth=1 https://github.com/missgoin/Anykernel3 AK3
 
 # Set PATH
 PATH="${KERNEL_DIR}/clang/bin:${PATH}"
@@ -77,14 +84,30 @@ function compile() {
 START=$(date +"%s")
 
 # Generate .config
-make O=out ARCH=arm64 ${DEFCONFIG} LLVM=1 LLVM_IAS=1
+make O=out ARCH=arm64 ${DEFCONFIG}
 
 # Start Compilation
 if [[ "$TOOLCHAIN" == "azure" || "$TOOLCHAIN" == "proton" ]]; then
      make -j$(nproc --all) O=out ARCH=arm64 CC=clang CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_COMPAT=arm-linux-gnueabi- LLVM=1 LLVM_IAS=1 V=$VERBOSE 2>&1 | tee error.log
 elif [[ "$TOOLCHAIN" == "nexus" ]]; then
      make -j$(nproc --all) O=out ARCH=arm64 CC=clang CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_COMPAT=arm-linux-gnueabi- LLVM=1 LLVM_IAS=1 V=$VERBOSE 2>&1 | tee error.log
+elif [[ "$TOOLCHAIN" == "neutron" ]]; then
+make -j$(nproc --all) O=out \
+   ARCH=$ARCH \
+			CC="clang" \
+			CROSS_COMPILE=aarch64-linux-gnu- \
+			CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+   LLVM=1 \
+			LD=ld.lld \
+			AR=llvm-ar \
+			NM=llvm-nm \
+			OBJCOPY=llvm-objcopy \
+			OBJDUMP=llvm-objdump \
+			STRIP=llvm-strip \
+			CONFIG_NO_ERROR_ON_MISMATCH=y \
+   V=$VERBOSE 2>&1 | tee error.log
 fi
+
 
 }
 
